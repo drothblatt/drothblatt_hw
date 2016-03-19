77f34eff45a0751149172088b54a700fb770ac76
David Rothblatt <daroth13@gmail.com>
2016-03-19 19:03:33 -0400

    added text to help switchover

--- hw05/index.js
+++ hw05/index.js
@@ -1,21 +1,30 @@
 var data = [4,8,15,16,23,42];
 var data2 = [6,19,8,20,35,62];
 
-var div_framework = document.getElementById("div_framework");
+var p = document.getElementById("p");
 var donkey = document.getElementById("donkey");
 var elephant = document.getElementById("elephant");
 
 var create_graph = function(){
+    p.innerHTML = "";
     if (donkey.checked && elephant.checked){
+	p.innerHTML = "Dem & Rep Delegates: <br>";
 	d3stuff(data);
 	d3stuff(data2);
     }else if (donkey.checked){
+	console.log("donkey");
+	p.innerHTML = "Democratic Delegates: <br>";
 	d3stuff(data);
     }else if (elephant.checked){
+	console.log("elephant");
+	p.innerHTML = "Republic Delegates: <br>";
 	d3stuff(data2);
     }else{
+	p.innerHTML = "Select a party to view delegate chart. <br>";
 	d3.select(".chart").selectAll("div").remove();
     }
+    console.log("p: " +  p);
+
 }
 
 var d3stuff = function(data){ 
