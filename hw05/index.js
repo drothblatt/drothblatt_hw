54f7b116932a4264bd4b547799e6cb0065d83282
David Rothblatt <daroth13@gmail.com>
2016-03-21 22:35:42 -0400

    fixed

--- hw05/index.js
+++ hw05/index.js
@@ -1,6 +1,15 @@
 var data = [4,8,15,16,23,42];
 var data2 = [6,19,8,20,35,62];
 
+var states = ["Alabama","Alaska","American Samoa","Arizona","Arkansas",
+  "California","Colorado","Connecticut","Delaware","Washington DC","Florida","Georgia",
+  "Guam","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine",
+  "Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana",
+  "Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina",
+  "North Dakota","North Marianas","Ohio","Oklahoma","Oregon","Pennsylvania","Puerto Rico",
+  "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virgin Islands",
+  "Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
+
 var dem = [44,24,35,53,53,32,66,102,91,77,38,67,222,
 	   16,95,33,51,25,25,130,36,214,156,71,107,143,
 	   75,23,33,16,25,101,86,14,247,55,21,95,189,24,
@@ -17,33 +26,34 @@ var elephant = document.getElementById("elephant");
 
 var create_graph = function(){
     d3.select(".chart").selectAll("div").remove();
-
     console.log("create_graph");
-    //console.log(opposite_party.checked);
-    //var x = document.getElementById(opposite_party);
-    //console.log("x: " + x);
-    //x.checked = false;
     p.innerHTML = "";
     if (donkey.checked){
 	console.log("donkey");
 	p.innerHTML = "Democratic Delegates: <br>  4,763 total Democratic delegates," + 
-	    "4,050 pledged delegates & 794 are superdelegates";
+	    " 4,050 pledged delegates & 794 are superdelegates";
 	d3stuff(dem);
     }else if (elephant.checked){
 	console.log("elephant");
-	p.innerHTML = "Republic Delegates: <br> 2,472 total Republican delegates, 1,719 being pledged delegates.";
+	p.innerHTML = "Republic Delegates: <br> 2,472 total Republican delegates," +
+	    " 1,719 being pledged delegates.";
 	d3stuff(rep);
     }
 }
 
-var d3stuff = function(data){ 
-    d3.select(".chart")
+var d3stuff = function(data){
+    d3.select("#p")
       .selectAll("div")
         .data(data)
       .enter().append("div")
    	.style("width", function(d) {return d*2 + "px"; })
-	.text(function(d) {return d;});
+	.text(function(d) {return d;})
+        .data(states)
+      .insert("span")
+      .style("color", "black")
+      .text(function(d){return " " + d;});
 }
+
 
 donkey.addEventListener('click',create_graph);
 elephant.addEventListener('click',create_graph);
