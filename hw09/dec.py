4d309f20fc890816de2c455c414a4b30cc0ebc49
David Rothblatt <daroth13@gmail.com>
2016-04-09 18:56:16 -0400

    progress on hw09

---
+++ hw09/dec.py
@@ -0,0 +1,28 @@
+
+def time_logging(f):
+    def inner( *args ):
+        import time
+        start_time = time.time()
+        run = f(*args)
+        time = time.time() - start_time
+        return "execution time: " + str(time) + "\n" + str(run)
+    return inner
+    
+
+def fname_logging(f):
+    def inner( *args ):
+        run = f(*args)
+        return "function name: " + f.func_name + "\n" + str(run)
+    return inner
+
+
+@time_logging
+@fname_logging
+def intersection(A, B):
+    return [x for x in A if x in A and x in B]
+
+A = [1, 2, 3]
+B = [2, 3, 4]
+print intersection(A, B)
+    
+
